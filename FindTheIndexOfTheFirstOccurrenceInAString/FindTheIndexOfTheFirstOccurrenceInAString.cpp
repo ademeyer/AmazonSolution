be05dc16ad7190d987c5c023aaef4ef184dee5a8
ademeyer <adebayotimileyin@gmail.com>
2024-04-17 17:30:37 +0100

    Fix FindTheIndex...InAString.cpp

--- FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.cpp
+++ FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -4,34 +4,30 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-/*
-    const int m = haystack.length();
-    const int n = needle.length();
-
-    for (int i = 0; i < m - n + 1; i++)
-      if (haystack.substr(i, n) == needle)
-        return i;
-
-*/
 
 vector<int> getTable(string needle)
 {
-    int i = 0, j = 0;
+    int i = 1, j = 0;
     vector<int> PiTable(needle.size(), 0);
-    for (i = 1; i < needle.size(); i++)
+    while (i < needle.size())
     {
         if (needle[i] == needle[j])
         {
-            PiTable[i] = ++j;
+            j++;
+            PiTable[i] = j;
+            i++;
         }
-        else //if (j > 0)
+        else
         {
-            while(j > 0 && needle[i] != needle[j])
+            if (j != 0)
                 j = PiTable[j - 1];
+            else
+            {
+                PiTable[i] = 0;
+                i++;
+            }
         }
-
     }
-    PiTable[i-1] = ++j;
 
     return PiTable;
 }
@@ -39,30 +35,29 @@ vector<int> getTable(string needle)
 int strStr(string haystack, string needle)
 {
     if (needle.size() > haystack.size()) return -1;
-    int index = -1, track = 0, i = 0;
+    int i = 0, j = 0;
     auto ans = getTable(needle);
 
-    while(i < haystack.size()) //for (int i = 0; i < haystack.size(); i++)
+    while(haystack.size() - i >= needle.size() - j)
     {
-
-        if (needle[track] != haystack[i])
-        {
-            if (track > 0)
-                track = ans[track - 1];
-            else 
-                i++;
-            index = -1;
-            //continue;
-        }
-        else
-        {
-            ++track;
-            if (index == -1) index = i;
-            if (track == needle.size()) return index;
+        if (needle[j] == haystack[i])
+        { 
             i++;
+            j++;
         }
-
-        
+        if (j == needle.size())
+        {
+            return i - j;
+            // j = ans[j-1];
+        }
+        // If mismatch occur after j matches
+        else if(i < haystack.size() && needle[j] != haystack[i]) 
+        {
+            if (j != 0) 
+                j = ans[j - 1];
+            else
+                i++;
+        }        
     }
 
     return -1;
@@ -71,8 +66,18 @@ int strStr(string haystack, string needle)
 
 int main()
 {
-   /* auto ans = getTable("abcdabca");
+   auto ans = getTable("abcdabca");
     for(int k = 0; k < ans.size(); k++)
+        std::cout << ans[k] << ",";
+    cout << "\n";
+
+    ans = getTable("AAACAAAA");
+    for (int k = 0; k < ans.size(); k++)
+        std::cout << ans[k] << ",";
+    cout << "\n";
+
+    ans = getTable("AAAA");
+    for (int k = 0; k < ans.size(); k++)
         std::cout << ans[k] << ",";
     cout << "\n";
 
@@ -84,14 +89,6 @@ int main()
     std::cout << strStr("sadbutsad", "sad") << "\n";
     std::cout << strStr("leetcode", "leeto") << "\n";
     std::cout << strStr("mississippi", "issip") << "\n";
-    std::cout << strStr("mississippi", "issi") << "\n";*/
-
-    const int data = 234;
-
-    const int* ptr = &data;
-
-    cout << "*ptr = " << *ptr << "\n";
-    *ptr++;
-    cout << "*ptr = " << *ptr << "\n";
+    std::cout << strStr("mississippi", "issi") << "\n";
 
 }
